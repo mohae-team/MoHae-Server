b73865adc7caeac48b18e4fdcb8ef0dbec3bb0c8
jinusong <sugwer7464@gmail.com>
2019-10-23 09:56:48 +0900

    [Update] gradle

--- src/main/java/com/mohaeyo/mohae/MoHaeServer/controller/BasicController.java
+++ src/main/java/com/mohaeyo/mohae/MoHaeServer/controller/BasicController.java
@@ -1,4 +1,14 @@
 package com.mohaeyo.mohae.MoHaeServer.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mohae")
 public class BasicController {
+    @GetMapping
+    String hello() {
+        return "hello world";
+    }
 }
